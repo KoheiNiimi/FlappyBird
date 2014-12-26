621cf1be8363d4e24be33216e783b757a8991952
niimi_kohei <niimi_kohei@cyberagent.co.jp>
2014-12-26 13:43:04 +0900

    lesson3 finished

---
+++ flappBird/Assets/BgLooper.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgLooper : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log ("Triggered:" + collider.name);
+	}
+}
