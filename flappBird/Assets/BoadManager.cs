42c28f04291f1674c7b05519493a6e8a8affe31b
niimi_kohei <niimi_kohei@cyberagent.co.jp>
2015-01-14 19:04:01 +0900

    パイプを動かした

---
+++ flappBird/Assets/BoadManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoadManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public GameObject boadObject;
+	
+	private float waitTime = 2.0f;
+
+	void Awake(){
+		// InvokeRepeating("関数名",初回呼出までの遅延秒数,次回呼出までの遅延秒数)
+		InvokeRepeating("Create", waitTime, waitTime);
+	}
+	/// <summary>
+	/// オブジェクトの生成
+	/// </summary>
+	void Create(){
+		// インスタンス生成
+		Instantiate(boadObject, new Vector3(2.5f, 1.7f, 0), Quaternion.identity);
+	}
+}
---
+++ flappBird/Assets/MoveBoad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBoad : MonoBehaviour {
+
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	void FixedUpdate () {
+		
+		rigidbody2D.velocity = -Vector2.right * speed;
+		
+	}
+}
