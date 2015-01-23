0e40d1b15d22a3e8f480fa6cb890a9ca7d26fd6d
niimi_kohei <niimi_kohei@cyberagent.co.jp>
2015-01-23 19:59:30 +0900

    最新版

--- flappBird/Assets/BirdMovement.cs
+++ flappBird/Assets/BirdMovement.cs
@@ -3,123 +3,121 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class BirdMovement : MonoBehaviour {
+public class BirdMovement : MonoBehaviour
+{
 
-	Vector3 velocity = Vector3.zero;
-	float flapSpeed = 100f;
-	float forwardSpeed = 1f;
+		Vector3 velocity = Vector3.zero;
+		float flapSpeed = 100f;
+		float forwardSpeed = 1f;
 
-	float defaultPlayerPositionX;
-	float defaultPlayerPositionY;
-	float defaultPlayerPositionZ;
+		float defaultPlayerPositionX;
+		float defaultPlayerPositionY;
+		float defaultPlayerPositionZ;
 
-	bool didFlap = false;
+		bool didFlap = false;
 
-	bool upBirdFlg = true;
-	bool downBirdFlg = false;
+		bool upBirdFlg = true;
+		bool downBirdFlg = false;
 
-	private bool gameover = false;
+		private bool gameover = false;
 
-	Animator animator;
+		Animator animator;
 
-	public List<Sprite> _numberImage;
+		public List<Sprite> _numberImage;
 
-	GameObject score0bject;
-	ScoreController scoreCon;
+		GameObject score0bject;
+		ScoreController scoreCon;
 
 
-	// Use this for initialization
-	void Start () {
-		animator = transform.GetComponentInChildren<Animator> ();
-		defaultPlayerPositionX = transform.position.x;
-		defaultPlayerPositionY = transform.position.y;
-		defaultPlayerPositionZ = transform.position.z;
-		score0bject = GameObject.Find("Score");
-		scoreCon = score0bject.GetComponent<ScoreController>();
-	}
-
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
-			didFlap = true;
+		// Use this for initialization
+		void Start ()
+		{
+				animator = transform.GetComponentInChildren<Animator> ();
+				defaultPlayerPositionX = transform.position.x;
+				defaultPlayerPositionY = transform.position.y;
+				defaultPlayerPositionZ = transform.position.z;
+				score0bject = GameObject.Find ("Score");
+				scoreCon = score0bject.GetComponent<ScoreController> ();
 		}
-	}
 
-	void FixedUpdate () {
+		void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+						didFlap = true;
+				}
+		}
 
-		if (Application.loadedLevelName == "TitleTop" || Application.loadedLevelName == "ReadyTop") {
-			animator.SetTrigger("DoFlap");
-			if (upBirdFlg) {
-				transform.position = new Vector3(transform.position.x,  transform.position.y + 0.005f, transform.position.z);
-			}
-			if (downBirdFlg) {
-				transform.position = new Vector3(transform.position.x,  transform.position.y - 0.005f , transform.position.z);
-			}
-			if (transform.position.y > defaultPlayerPositionY + 0.03f) {
-				downBirdFlg = true;
-				upBirdFlg = false;
-			}
-			if(transform.position.y < defaultPlayerPositionY - 0.03f){
-				upBirdFlg = true;
-				downBirdFlg = false;
-			}
+		void FixedUpdate ()
+		{
 
-		} else {
+				if (Application.loadedLevelName == "TitleTop" || Application.loadedLevelName == "ReadyTop") {
+						animator.SetTrigger ("DoFlap");
+						if (upBirdFlg) {
+								transform.position = new Vector3 (transform.position.x, transform.position.y + 0.004f, transform.position.z);
+						}
+						if (downBirdFlg) {
+								transform.position = new Vector3 (transform.position.x, transform.position.y - 0.004f, transform.position.z);
+						}
+						if (transform.position.y > defaultPlayerPositionY + 0.04f) {
+								downBirdFlg = true;
+								upBirdFlg = false;
+						}
+						if (transform.position.y < defaultPlayerPositionY - 0.04f) {
+								upBirdFlg = true;
+								downBirdFlg = false;
+						}
 
-			if (didFlap) {
-				rigidbody2D.AddForce (Vector2.up * flapSpeed);
+				} else {
 
-				animator.SetTrigger("DoFlap");
+						if (didFlap) {
+								rigidbody2D.AddForce (Vector2.up * flapSpeed);
 
-				didFlap = false;
-			}
+								animator.SetTrigger ("DoFlap");
 
-			if (rigidbody2D.velocity.y > 0) {
-				transform.rotation = Quaternion.Euler (0, 0, 0);
-			} else {
+								didFlap = false;
+						}
+
+						if (rigidbody2D.velocity.y > 0) {
+								transform.rotation = Quaternion.Euler (0, 0, 0);
+						} else {
 //			float angle = Mathf.Lerp(0, -90, -rigidbody2D.velocity.y);
 //			transform.rotation = Quaternion.Euler(0, 0, angle);
-			}
+						}
+
+				}
+
 
 		}
 
+		// スコア用
+		private int score = 0;
+		// スコアの表示の設定を行います。
+		public GUIStyle guiStyle;
+		// トリガーに入った時に score を加算します。
+		void OnTriggerEnter2D (Collider2D collider)
+		{
+				score += 1;
+				scoreCon.UpdsateScore (score);
+		}
 
-	}
+		// 何かにぶつかったら呼ばれる
+		void OnCollisionEnter2D (Collision2D collision)
+		{
+				StartCoroutine (GameOver ());
+		}
 
-	// スコア用
-	private int score = 0;
-	// スコアの表示の設定を行います。
-	public GUIStyle guiStyle;
-	// トリガーに入った時に score を加算します。
-	void OnTriggerEnter2D(Collider2D collider) {
-		score += 1;
-		scoreCon.UpdsateScore (score);
-		Destroy(collider); // OnTriggerEnter は1回しか呼ばれない、という認識なんですが何度も呼ばれてウボァーってなったので Destroy 呼んでます。
-	}
-
-	void OnGUI () {
-		if (Application.loadedLevelName != "TitleTop" && Application.loadedLevelName != "ReadyTop") {
-						if (gameover) {
-								GUI.Label (new Rect (0, 0, Screen.width, Screen.height), "gameover\nscore:" + score.ToString (), guiStyle);
-						} else {
-								GUI.Label (new Rect (0, 0, 200, 50), "score:" + score.ToString (), guiStyle);
-						}
+		IEnumerator GameOver ()
+		{
+				// ゲームオーバーのフラグをたてる
+				gameover = true;
+				// マウス連打してたらスコアが表示されずに画面が遷移するのでその対策
+				yield return new WaitForSeconds (1f);
+				// マウスクリックしたらゲームの最初に戻る
+				while (!Input.GetMouseButtonDown(0)) {
+						yield return 0;
 				}
-	}
-
-	// 何かにぶつかったら呼ばれる
-	void OnCollisionEnter2D(Collision2D collision) {
-		StartCoroutine(GameOver());
-	}
-
-	IEnumerator GameOver() {
-		// ゲームオーバーのフラグをたてる
-		gameover = true;
-		// マウス連打してたらスコアが表示されずに画面が遷移するのでその対策
-		yield return new WaitForSeconds(1f);
-		// マウスクリックしたらゲームの最初に戻る
-		while (!Input.GetMouseButtonDown(0)) { yield return 0; }
-		Application.LoadLevel("Main");
-	}
+				Application.LoadLevel ("Main");
+		}
 
 
 
---
+++ flappBird/Assets/SceneToMainController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneToMainController : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+
+				if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+						Application.LoadLevel ("Main"); //　シーン遷移
+				}
+	
+		}
+}
